788e3b3948740c21ccfe3c8185af4f339c74dec7
phenry <phenry@SOLO-WS028>
2020-11-13 16:39:20 -0500

    initial setup

---
+++ CasinoFileProcesser/Modal/MatrixList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoFileProcesser.Modal
+{
+    public class MatrixList
+    {
+        public string Segment { get; set; }
+        public int Monthly { get; set; }
+        public int MediaCoupon { get; set; }
+        public string MediaCategory { get; set; }
+        public int FP { get; set; }
+        public string Valid { get; set; }
+    }
+}
--- CasinoFileProcesser/Program.cs
+++ CasinoFileProcesser/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CasinoFileProcesser.Modal;
+using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,72 @@ namespace CasinoFileProcesser
     {
         static void Main(string[] args)
         {
+            Processer();
+        }
+
+        private static void Processer()
+        {
+            System.Data.DataTable addressList = new System.Data.DataTable();
+            string pathName = @"C:\Projects\Casino\Data.xlsx";
+            string sheetName = "Sheet1";
+
+            using (OleDbConnection connection =
+                    new OleDbConnection((pathName.TrimEnd().ToLower().EndsWith("x"))
+                    ? "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + pathName + "';" + "Extended Properties='Excel 12.0 Xml;HDR=YES;'"
+                    : "provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + pathName + "';Extended Properties=Excel 8.0;"))
+            {
+                OleDbDataAdapter data = new OleDbDataAdapter(string.Format("select * from [{0}$]", sheetName), connection);
+
+                data.Fill(addressList);
+
+                List<MatrixList> matrix = GetMatrixTableData();
+
+                if (addressList.Rows.Count > 0)
+                {
+                    foreach (DataRow row in addressList.Rows)
+                    {
+                        
+                    }
+                }
+            }
+        }
+
+        private static List<MatrixList> GetMatrixTableData()
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+            string pathName = @"C:\Projects\Casino\Matrix.xlsx";
+            string sheetName = "Sheet2";
+
+            using (OleDbConnection connection =
+                    new OleDbConnection((pathName.TrimEnd().ToLower().EndsWith("x"))
+                    ? "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + pathName + "';" + "Extended Properties='Excel 12.0 Xml;HDR=YES;'"
+                    : "provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + pathName + "';Extended Properties=Excel 8.0;"))
+            {
+                OleDbDataAdapter data = new OleDbDataAdapter(string.Format("select * from [{0}$]", sheetName), connection);
+
+                data.Fill(dt);                
+            }
+
+            List<MatrixList> matrixList = new List<MatrixList>();
+
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    matrixList.Add(new MatrixList()
+                    {
+                        Segment = row[0].ToString(),
+                        Monthly = int.Parse(row[1].ToString()),
+                        MediaCoupon = int.Parse(row[2].ToString()),
+                        MediaCategory = row[3].ToString(),
+                        FP = int.Parse(row[4].ToString()),
+                        Valid = row[5].ToString()
+                    });
+                }
+            }
+
+            return matrixList;
+
         }
     }
 }
